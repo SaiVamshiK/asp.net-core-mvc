24db827e55f3f9dc5f488d84535851729bd14f85
SaiVamshiK <saivamshi.k.24@gmail.com>
2021-06-25 21:35:15 +0530

    edit functionality added

--- MvcCourse/Controllers/CategoryController.cs
+++ MvcCourse/Controllers/CategoryController.cs
@@ -42,5 +42,43 @@ namespace MvcCourse.Controllers
             }
             
         }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null||(id==0))
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if(obj==null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(obj);
+            }
+
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
     }
 }
