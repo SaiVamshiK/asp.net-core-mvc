ffc70bca73984681b44084853f32c8f0060a04d2
SaiVamshiK <saivamshi.k.24@gmail.com>
2021-06-25 17:14:14 +0530

    retrieved category table from db and passed it to view

--- MvcCourse/Controllers/CategoryController.cs
+++ MvcCourse/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCourse.Data;
+using MvcCourse.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,15 @@ namespace MvcCourse.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> obj = _db.Category;
+            return View(obj);
         }
     }
 }
